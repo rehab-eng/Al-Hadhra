ab6f7678ae33f802e41000e270db6df36b198fa7
Mohamed <hsjshdvehhs@gmail.com>
2026-02-16 17:06:19 +0200

    Allow custom puppeteer executable path

--- backend/src/common/pdf/pdf.service.ts
+++ backend/src/common/pdf/pdf.service.ts
@@ -164,6 +164,7 @@ export class PdfService {
     `;
 
     const browser = await puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
